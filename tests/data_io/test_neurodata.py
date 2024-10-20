a36e94ba4373618a2e6b95d5a094d31133774782
Gabriel <dellabellagabriel@gmail.com>
2024-10-20 20:16:25 -0300

    add license text to files

--- tests/data_io/test_neurodata.py
+++ tests/data_io/test_neurodata.py
@@ -1,3 +1,7 @@
+# License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
+# Copyright (c) 2024, Della Bella, Gabriel; Rodriguez, Natalia
+# All rights reserved.
+
 from neurostates.data_io.matrix.matrix import create_data_from_matrix
 
 import numpy as np
