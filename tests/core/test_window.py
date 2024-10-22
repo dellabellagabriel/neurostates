bd232a584c48b58564dfe17c679b035eacba324e
Gabriel <dellabellagabriel@gmail.com>
2024-10-22 17:58:42 -0300

    fix test of sliding window

--- tests/core/test_window.py
+++ tests/core/test_window.py
@@ -19,7 +19,7 @@ def test_sliding_window_no_tapering_function():
     sliding_window.apply_to(neurodata)
 
     n_windows_ref = int((n_samples - size) / step) + 1
-    assert sliding_window.windowed_data_.windows == n_windows_ref
-    assert sliding_window.windowed_data_.subjects == n_subjects
-    assert sliding_window.windowed_data_.regions == n_regions
-    assert sliding_window.windowed_data_.samples == n_samples
+    assert sliding_window.windows == n_windows_ref
+    assert sliding_window.subjects == n_subjects
+    assert sliding_window.regions == n_regions
+    assert sliding_window.samples == size
