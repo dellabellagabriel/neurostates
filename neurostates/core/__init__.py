78dd59cec25a053cdb26646c26869e2ec21ab49c
Gabriel <dellabellagabriel@gmail.com>
2024-10-22 17:18:25 -0300

    add module docstrings to package and core module

--- neurostates/core/__init__.py
+++ neurostates/core/__init__.py
@@ -2,5 +2,5 @@
 # Copyright (c) 2024, Della Bella, Gabriel; Rodriguez, Natalia
 # All rights reserved.
 
-"""Neurostates is a tool for analyzing patterns of functional connectivity in\
-EEG and fMRI."""
+
+"""Core functionalities and structures of neurostates."""
