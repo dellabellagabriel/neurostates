00e850a3cb71209f13bb77f33d7783f7ae01947b
Natalia Rodriguez <natalia.agustina.rodriguez@mi.unc.edu.ar>
2024-11-27 17:44:58 -0300

    remove classes in window and connectivity

--- neurostates/core/connectivity.py
+++ neurostates/core/connectivity.py
@@ -45,37 +45,3 @@ def connectivity(windowed_data_raw, method=None):
             )
 
     return connectivity_data
-    
-
-class Connectivity:
-    """
-    Represents the functional connectivity operation. It is applied to objects\
-    of type SlidingWindow.
-
-    Parameters
-    ----------
-    method: callable
-        The function that will be used to compute the connectivity between
-        regions
-    """
-
-    def __init__(self, method):
-        self.method = method
-
-    def apply_to(self, slidingwindow):
-        """Apply the connectivity operation to a SlidingWindow object."""
-        connectivity_data = np.empty(
-            (
-                slidingwindow.windows,
-                slidingwindow.subjects,
-                slidingwindow.regions,
-                slidingwindow.regions,
-            )
-        )
-        for window in range(slidingwindow.windows):
-            for subject in range(slidingwindow.subjects):
-                connectivity_data[window, subject, :, :] = self.method(
-                    slidingwindow.windowed_data_[window, subject, :, :]
-                )
-
-        self.connectivity_data_ = connectivity_data
