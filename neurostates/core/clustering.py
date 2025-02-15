256e7b701c78d9a03587a33b86b7b297ce84a805
Gabriel <dellabellagabriel@gmail.com>
2025-02-15 19:51:25 -0300

    add frequencies transformer

--- neurostates/core/clustering.py
+++ neurostates/core/clustering.py
@@ -33,49 +33,6 @@ class Concatenator(BaseEstimator, TransformerMixin):
             )
             data_concatenate.append(data)
 
-        data_concatenate = np.array(data_concatenate)
-        data_concatenate = data_concatenate.reshape(-1, rois**2)
+        data_concatenate = np.concatenate(data_concatenate, axis=0)
 
         return data_concatenate
-
-
-def clustering(groups_dict, n_clusters, **clustering_kwargs):
-    """Perform k-means clustering on dynamic connectivity matrices.
-
-    This function uses the groups found in `groups_dict` as the\
-    groups/conditions of the experiment and returns an\
-    n_clusters x rois x rois matrix.
-
-    Parameters
-    ----------
-        groups_dict (dict[str, ndarray]): A dictionary of conditions.
-        Each condition must have a numpy array with the dynamic connectivity
-        matrices from connectivity().
-
-        n_clusters (int): The number of clusters.
-        **clustering_kwargs: Keyword arguments for clustering.
-
-    Returns
-    -------
-        centroids: ndarray
-        An array of size n_centroids x regions x regions that results from\
-        the clustering algorithm.
-    """
-    validate_groups_dict(groups_dict)
-
-    data_concatenate = []
-    for group in groups_dict:
-        subjects, windows, rois, _ = groups_dict[group].shape
-        data = (
-            groups_dict[group]
-            .reshape(subjects, windows, -1)
-            .reshape(subjects * windows, -1)
-        )
-        data_concatenate.append(data)
-
-    data_concatenate = np.array(data_concatenate)
-    data_concatenate = data_concatenate.reshape(-1, rois**2)
-    kmeans = KMeans(n_clusters=n_clusters, **clustering_kwargs)
-    kmeans.fit(data_concatenate)
-
-    return kmeans.cluster_centers_.reshape(-1, rois, rois)
