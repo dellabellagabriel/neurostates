dca8f603e270e780fbac50ac5113a3b4d02f1ccd
Gabriel <dellabellagabriel@gmail.com>
2025-02-16 16:36:23 -0300

    add clustering documentation

--- neurostates/core/clustering.py
+++ neurostates/core/clustering.py
@@ -13,13 +13,51 @@ from .utils import validate_groups_dict
 
 
 class Concatenator(BaseEstimator, TransformerMixin):
+    """Concatenate the dynamic connectivity matrices by subjects\
+    and windows for the clustering step."""
+
     def __init__(self):
         pass
 
-    def fit(self, X):  # noqa: N803
+    def fit(self, X, y=None):  # noqa: N803
+        """
+        This method does nothing but is required by the scikit-learn\
+        interface.
+        No parameters are fit in this transformer.
+
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Input data to fit the transformer.
+        y : array-like, shape (n_samples,), default=None
+            Target labels (not used in this case).
+
+        Returns
+        -------
+        self : object
+            The fitted transformer (no changes in this case).
+        """
         return self
 
     def transform(self, X):  # noqa: N803
+        """
+        Transforms a numpy array of size\
+        n_subjects x n_windows x n_rois x n_rois to\
+        n_subjects*n_windows x n_rois*n_rois.
+
+        Parameters
+        ----------
+        X : ndarray
+            A dynamic connectivity matrix of size\
+            n_subjects x n_windows x n_rois x n_rois
+
+        Returns
+        -------
+        data_concatenate : ndarray
+            A concatenated matrix of size\
+            n_subjects*n_windows x n_rois*n_rois
+        """
+
         validate_groups_dict(X)
 
         data_concatenate = []
