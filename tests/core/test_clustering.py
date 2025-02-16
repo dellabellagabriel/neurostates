cc9d0321c59127f08ea992994ee67d0b454f92a2
Natalia Rodriguez <natalia.agustina.rodriguez@mi.unc.edu.ar>
2025-02-16 14:34:03 -0300

    test concatenator

--- tests/core/test_clustering.py
+++ tests/core/test_clustering.py
@@ -1,11 +1,42 @@
-from neurostates.core.clustering import clustering
-from neurostates.core.connectivity import connectivity
-from neurostates.core.window import window
+from neurostates.core.clustering import Concatenator, clustering
+from neurostates.core.connectivity import DynamicConnectivity, connectivity
+from neurostates.core.window import SamplesWindower, window
 
 import numpy as np
 
 import pytest
 
+from scipy.signal.windows import hamming
+
+
+def test_concatenator():
+    np.random.seed(42)
+    n_subjects = 20
+    n_regions = 90
+    n_samples = 150
+    matrix = np.random.rand(n_subjects, n_regions, n_samples)
+    
+    length_in_samples = 20
+    step_in_samples = 5
+    
+    sliding_windower = SamplesWindower(
+        length=length_in_samples,
+        step=step_in_samples,
+        tapering_function=hamming,
+    )
+    
+    sliding_window = sliding_windower.transform(matrix)
+    dynamic_connectivity = DynamicConnectivity()
+    connectivity_output = dynamic_connectivity.transform(sliding_window)
+    
+    groups_dict = {
+        "group_a": connectivity_output,
+        "group_b": connectivity_output
+    }
+    
+    concatenated = Concatenator()
+    concatenated_shape = concatenated.transform(groups_dict).shape
+    assert concatenated_shape == (1080,8100)
 
 def test_clustering():
     np.random.seed(42)
