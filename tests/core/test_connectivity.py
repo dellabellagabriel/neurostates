eba4d8e7b501d3b2c62f87a2814cda3c664e7fd0
Gabriel <dellabellagabriel@gmail.com>
2025-04-05 18:50:39 -0300

    fix black8 format

--- tests/core/test_connectivity.py
+++ tests/core/test_connectivity.py
@@ -5,9 +5,10 @@
 from pathlib import Path
 
 from neurostates.core.connectivity import (
-    DynamicConnectivity, DynamicConnectivityGroup, connectivity
+    DynamicConnectivity,
+    DynamicConnectivityGroup,
+    connectivity,
 )
-
 from neurostates.core.window import (
     SamplesWindower,
     SamplesWindowerGroup,
@@ -94,17 +95,18 @@ def test_dynamic_connectivity_shape():
 
     assert connectivity_output == (20, 27, 90, 90)
 
+
 def test_dynamic_connectivity_group_shape():
     np.random.seed(42)
     n_subjects = 20
     n_regions = 90
     n_samples = 150
-    
+
     group_of_matrices = {
         "matrix1": np.random.rand(n_subjects, n_regions, n_samples),
-        "matrix2": np.random.rand(n_subjects, n_regions, n_samples)
+        "matrix2": np.random.rand(n_subjects, n_regions, n_samples),
     }
-    
+
     length_in_samples = 20
     step_in_samples = 5
     sliding_windower = SamplesWindowerGroup(
@@ -115,13 +117,14 @@ def test_dynamic_connectivity_group_shape():
     sliging_window = sliding_windower.transform(group_of_matrices)
 
     dynamic_connectivity = DynamicConnectivityGroup()
-    
+
     transformed = dynamic_connectivity.transform(sliging_window)
-    
+
     for matrix, data in transformed.items():
         connectivity_output = data.shape
 
         assert connectivity_output == (20, 27, 90, 90)
+
 
 def test_dynamic_connectivity_seconds_windower():
     path_to_tests = Path("tests/core")
